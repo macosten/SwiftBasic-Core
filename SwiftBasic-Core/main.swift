09b798d039b5a5e82593da7f7c2ed1d927f883b1
Zaccari S <zacosx@gmail.com>
2020-01-04 22:26:17 -0500

    Initial Commit

---
+++ SwiftBasic-Core/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftBasic-Core
+//
+//  Created by Zaccari Silverman on 1/4/20.
+//  Copyright © 2020 Zaccari Silverman. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
